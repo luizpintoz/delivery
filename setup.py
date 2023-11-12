d7ef0620673a37e919837fa1b6e742ee18cd38a2
luizpintoz <yukidesu021@gmail.com>
2023-11-12 18:13:33 -0300

    modfic setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="delivery",
     version="0.1.0 ",
     description="delivery app",
-    packages=find_packages(exclude="../.venv"),
+    packages=find_packages(exclude=".venv/"),
     include_package_data=True,
     install_requires=read("requirements.txt"),
     extras_requires={
